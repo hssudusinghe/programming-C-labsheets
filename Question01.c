b74795595b02dda70454fbd9a84dd2ae7af11893
hssudusinghe <135609482+hssudusinghe@users.noreply.github.com>
2023-08-04 11:00:35 +0530

    Add files via upload

--- Question01.c
+++ Question01.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
-int main()
-{
-int n1,n2,max;
-printf("Enter two numbers");
-scanf("%d %d",&n1,&n2);
-if(n1>n2)
-max=n1;
-else
-max=n2;
-printf("The highest number is %d\n",max);
-return 0;
+
+int main() {
+    int arr[10];
+    int i;
+    int min, max;
+    double sum = 0.0;
+
+    // Input values to the array
+    printf("Enter 10 integer values:\n");
+    for (i = 0; i < 10; i++) {
+        printf("Value %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    // Find minimum and maximum values
+    min = arr[0];
+    max = arr[0];
+    for (i = 1; i < 10; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    // Calculate the sum for average
+    for (i = 0; i < 10; i++) {
+        sum += arr[i];
+    }
+
+    // Calculate the average
+    double average = sum / 10.0;
+
+    // Print the results
+    printf("Minimum value: %d\n", min);
+    printf("Maximum value: %d\n", max);
+    printf("Average value: %.2f\n", average);
+
+    // Reverse order of values
+    printf("Reverse order of values: ");
+    for (i = 9; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
 }
