58d1ced97e9b25b8569da5650806733ddadb858f
hssudusinghe <135609482+hssudusinghe@users.noreply.github.com>
2023-08-04 10:52:31 +0530

    Add files via upload

---
+++ q4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char letter;
+    printf("Enter a character:");
+    scanf("%c", &letter);
+    switch(letter)
+    {
+        case'a':
+        case'A':
+        case'e':
+        case'E':
+        case'i':
+        case'I':
+        case'o':
+        case'O':
+        case'u':
+        case'U':
+            printf("%c is a vowel.\n,", letter);
+            break;
+        default:
+            printf("%c is not a vowel.\n", letter);
+            break;
+
+    }
+    return 0;
+}
